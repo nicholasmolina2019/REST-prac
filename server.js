7f5fb8a32e7a01af2343d466e860bf6a35f7a1d2
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-17 09:49:27 -0500

    added array

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // require dependencies
-const express = require('express');
-const app = express();
+const express = require("express")
+const app = express()
 // express web server
 const port = 3000;
 
-
-app.listen(3000, () =>{
-
-console.log(`Listening to port 3000`, port)
+// route goes here
+const fruits = ["apple", "banana", "pear"];
+app.get("/fruits", (req, res) => {
+  res.send(fruits);
 });
 
-
-
-
+app.listen(3000, () => {
+  console.log(`Listening to port 3000`, port);
+});
