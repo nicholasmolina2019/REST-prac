9adda13c925c38af0c9559e8c9b5d2c2595b42c9
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-17 10:12:57 -0500

    finished

--- server.js
+++ server.js
@@ -5,11 +5,32 @@ const app = express()
 const port = 3000;
 
 // route goes here
-const fruits = ["apple", "banana", "pear"];
+const fruits = [
+    {
+      name: "apple",
+      color: "red",
+      readyToEat: true,
+    },
+    {
+      name: "pear",
+      color: "green",
+      readyToEat: false,
+    },
+    {
+      name: "banana",
+      color: "yellow",
+      readyToEat: true,
+    },
+  ]
 app.get("/fruits", (req, res) => {
   res.send(fruits);
 });
+//added show route
+app.get("/fruits/:indexOfFruitsArray",  (req, res) => {
 
+res.send(fruits[req.params.indexOfFruitsArray])
+
+})
 app.listen(3000, () => {
   console.log(`Listening to port 3000`, port);
 });
