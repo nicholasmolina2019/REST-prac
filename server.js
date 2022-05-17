25e5dcf7ef92968ca8c67403d285e2c18bfd3030
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-17 09:44:56 -0500

    added port console

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-
+// require dependencies
 const express = require('express');
 const app = express();
-
+// express web server
 const port = 3000;
+
+
+app.listen(3000, () =>{
+
+console.log(`Listening to port 3000`, port)
+});
+
+
+
+
