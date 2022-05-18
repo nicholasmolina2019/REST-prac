cc79cd4136898d408bc3ab63f08e48804bf2fc3a
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-18 09:32:57 -0500

    updated

---
+++ models/fruits.js
@@ -0,0 +1,22 @@
+// route goes here
+const fruits = [
+    {
+      name: "apple",
+      color: "red",
+      readyToEat: true,
+    },
+    {
+      name: "pear",
+      color: "green",
+      readyToEat: false,
+    },
+    {
+      name: "banana",
+      color: "yellow",
+      readyToEat: true,
+    },
+  ];
+
+  
+  module.exports = fruits;
+
--- server.js
+++ server.js
@@ -1,36 +1,51 @@
 // require dependencies
-const express = require("express")
-const app = express()
-// express web server
+const express = require('express');
+const fruits = require('./models/fruits');
+
+// initialize express application
+const app = express();
+
+// configure application settings
 const port = 3000;
 
-// route goes here
-const fruits = [
-    {
-      name: "apple",
-      color: "red",
-      readyToEat: true,
-    },
-    {
-      name: "pear",
-      color: "green",
-      readyToEat: false,
-    },
-    {
-      name: "banana",
-      color: "yellow",
-      readyToEat: true,
-    },
-  ]
-app.get("/fruits", (req, res) => {
-  res.send(fruits);
-});
-//added show route
-app.get("/fruits/:indexOfFruitsArray",  (req, res) => {
 
-res.send(fruits[req.params.indexOfFruitsArray])
+
+// mount middleware
+
+// mount routes
+
+// Index
+app.get('/fruits', (req, res) => {
+    res.render('index.ejs', { allFruits: fruits });
+                            // ^-- this is just a descriptive way of referencing our fruits array inside index.ejs
+});
+
+// N
+app.get("fruits/new", (req, res)=>{
+res.render("new.ejs")
 
 })
-app.listen(3000, () => {
-  console.log(`Listening to port 3000`, port);
+
+// D
+
+// U
+
+// C
+
+// E
+
+// Show
+app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+    // render is a special method that
+    // informs the template engine to render a template
+    // we just provide the name as a string
+    res.render('show.ejs', {
+        fruit: fruits[req.params.indexOfFruitsArray] // this references a single fruit
+        // and passes it to the template so we can access it there
+    });
+});
+
+// tell the app to listen
+app.listen(port, () =>{
+    console.log(`Listening on port`, port)
 });
